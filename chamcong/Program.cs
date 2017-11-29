2e956b609661955a05c350441d2c856177a8ef91
huytran <tranxuanhuy1@gmail.com>
2017-11-29 22:08:59 +0700

    dang lam cham cong thuc te

--- chamcong/Program.cs
+++ chamcong/Program.cs
@@ -10,6 +10,37 @@ namespace chamcong
     {
         static void Main(string[] args)
         {
+            //chamconglythuyet();
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\myexcel1.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+            List<DateTime> gioquetvantayThucte = new List<DateTime>();
+            for (int i = 9; i <= 28; i++)
+            {
+                string ngaychuadinhdang = xlRange.Cells[i, 5].Value2.ToString();
+                double date = double.Parse(ngaychuadinhdang);
+
+                var ngay = DateTime.FromOADate(date).ToString("dd/MM/yyyy");
+                string gio = xlRange.Cells[i, 6].Value2.ToString();
+                string[] cacgio = gio.Replace("\n","").Split(';');
+                foreach (var item in cacgio)
+                {
+                    gioquetvantayThucte.Add(new DateTime(int.Parse(ngay.Split('/')[2]), int.Parse(ngay.Split('/')[1]), int.Parse(ngay.Split('/')[0]), int.Parse(item.Split(':')[0]), int.Parse(item.Split(':')[1]), 0)); 
+                }
+             }
+            using (System.IO.StreamWriter file =
+          new System.IO.StreamWriter(@"C:\myexcel1.txt", false))
+            {
+                file.WriteLine(string.Join("\n", gioquetvantayThucte));
+            }
+        }
+
+        private static void chamconglythuyet()
+        {
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\myexcel.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
@@ -18,40 +49,44 @@ namespace chamcong
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
 
-            string gioquetvantayLythuyet = "";
             bool[] data1rowtungca = new bool[4 * 31];
 
             //xuat 1 ngay 4 ca la 4 gia tri bool
             for (int i = 10; i <= 10; i++)
             {
-                int x=0;
+                int x = 0;
                 for (int j = 4; j <= 33; j++)
                 {
                     string temp = xlRange.Cells[i, j].Value2.ToString();
                     if (temp.Contains("S")) data1rowtungca[x] = true;
-                    if (temp.Contains("T")) data1rowtungca[x+1] = true;
-                    if (temp.Contains("C")) data1rowtungca[x+2] = true;
-                    if (temp.Contains("D")) data1rowtungca[x+3] = true;
+                    if (temp.Contains("T")) data1rowtungca[x + 1] = true;
+                    if (temp.Contains("C")) data1rowtungca[x + 2] = true;
+                    if (temp.Contains("D")) data1rowtungca[x + 3] = true;
                     x += 4;
                 }
-                           }
- 
-            using (System.IO.StreamWriter file =
-           new System.IO.StreamWriter(@"C:\myexcel.txt", false))
-            {
-                file.WriteLine(string.Join("\n",data1rowtungca));
             }
 
             //xuat ra gio dang le phai quet van tay theo ly thuyet
             bool giatridangco = false;
-               for (int j = 0; j <124; j++)
-               {
-                   //truong hop len hoac xuong ca
-                   if(data1rowtungca[j]!=giatridangco)
-                   {
-                       if(data1rowtungca[j]%4==0) 
-                   }
-               }
+            List<DateTime> gioquetvantayLythuyet = new List<DateTime>();
+            for (int j = 0; j < 124; j++)
+            {
+                //truong hop len hoac xuong ca
+                if (data1rowtungca[j] != giatridangco)
+                {
+                    giatridangco = !giatridangco;
+                    if (j % 4 == 0) gioquetvantayLythuyet.Add(new DateTime(2017, 11, j / 4 + 1, 0, 00, 0));
+                    if (j % 4 == 1) gioquetvantayLythuyet.Add(new DateTime(2017, 11, j / 4 + 1, 6, 00, 0));
+                    if (j % 4 == 2) gioquetvantayLythuyet.Add(new DateTime(2017, 11, j / 4 + 1, 12, 00, 0));
+                    if (j % 4 == 3) gioquetvantayLythuyet.Add(new DateTime(2017, 11, j / 4 + 1, 18, 00, 0));
+                }
+            }
+
+            using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(@"C:\myexcel.txt", false))
+            {
+                file.WriteLine(string.Join("\n", gioquetvantayLythuyet));
+            }
         }
     }
 }
